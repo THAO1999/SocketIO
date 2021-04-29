6b14ff14871c472949ce6f7615e5f46fa6d4ac66
ThaoTM <thaotm@2nf.com.vn>
2021-04-29 17:09:11 +0700

    f

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.set("views", "./views");
 var server = require("http").Server(app)// dựng server để trả trang web về
 server.listen(3000)// chạy server port 3000
 
+var io = require("socket.io")(server);
+// lắng nghe client kết nối lên
+io.on("connection", function (socket) {
+    //biến socket để quản lí kết nối của từng client, biết được ai với ai vì mỗi socket đều có id
+    console.log("có người kết nối" + socket.id);
+    socket.on("disconnect", function () {
+        console.log("ngắt kết nối: " + socket.id);
+    })
+});
 app.get("/", function (req, res) {
-    res.render("trangchu");
+    res.render("home");
 });
